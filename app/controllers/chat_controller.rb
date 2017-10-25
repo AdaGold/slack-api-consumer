ee4c67c1046033ca5f1c276b28571a9b7094d766
Kari Bancroft <kariabancroft@gmail.com>
2017-10-25 12:26:44 -0700

    add the controller implementation

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,10 +1,21 @@
 class ChatController < ApplicationController
   def index
+    @channels = SlackApiWrapper.list_channels
   end
 
   def new
+    @channel = params[:channel]
   end
 
   def create
+    success = SlackApiWrapper.send_msg(params[:channel], params[:message])
+    if success
+      flash[:status] = :success
+      flash[:message] = "Message successfully sent"
+    else
+      flash[:status] = :error
+      flash[:message] = "Error sending the message"
+    end
+    redirect_to root_path
   end
 end
