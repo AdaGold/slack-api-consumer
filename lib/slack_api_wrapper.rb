e4726c08c9df0f4299964f469b6f3065c524ec1e
Kari Bancroft <kariabancroft@gmail.com>
2017-10-25 11:22:29 -0700

    add copied slack api wrapper code

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -1,5 +1,33 @@
-require `httparty`
+# require `httparty`
 
 class SlackApiWrapper
-  # Your code here!
+  BASE_URL = "https://slack.com/api/"
+  TOKEN = ENV["SLACK_TOKEN"]
+
+  def self.list_channels
+    url = BASE_URL + "channels.list?" + "token=#{TOKEN}" + "&exclude_archived=1"
+    data = HTTParty.get(url)
+    if data["channels"]
+      return data["channels"]
+    else
+      return []
+    end
+  end
+
+  def self.send_msg(channel, msg, token = nil)
+    token = TOKEN if token == nil
+    puts "Sending message to channel #{channel}: #{msg}"
+
+    url = BASE_URL + "chat.postMessage?" + "token=#{token}"
+
+    data = HTTParty.post(url,
+    body:  {
+      "text" => "#{msg}",
+      "channel" => "#{channel}",
+      "username" => "Roberts-Robit",
+      "icon_emoji" => ":robot_face:",
+      "as_user" => "false"
+    },
+    :headers => { 'Content-Type' => 'application/x-www-form-urlencoded' })
+  end
 end
