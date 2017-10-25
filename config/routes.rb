d752d11102a53eec157802798405ba372ccfc589
Kari Bancroft <kariabancroft@gmail.com>
2017-10-25 10:36:53 -0700

    modify routes and views to actually work

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'chats/index'
-
-  get 'chats/new'
-
-  get 'chats/create'
+  root 'chat#index'
+  get ':channel/new', to: 'chat#new', as: 'chat_new'
+  post ':channel', to: 'chat#create', as: 'chat_create'
 end
