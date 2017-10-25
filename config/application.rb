01eb8988b399752fa581cb95e5c63c0e1d3950ef
Kari Bancroft <kariabancroft@gmail.com>
2017-10-25 10:02:54 -0700

    add routes, stub files and wrapper class file

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module SlackAPI
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Automatically load all code from <rails_root>/lib
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
